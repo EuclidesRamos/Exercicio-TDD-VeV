17e096d643ac8d790e6b9de599a7a19959206fe1
EuclidesRamos <euclides.filho@ccc.ufcg.edu.br>
2022-10-22 01:08:57 -0300

    criando teste de unidade para verificar a criação de uma fatura

--- FiltroFaturas-EuclidesRamos/tests/Testes.java
+++ FiltroFaturas-EuclidesRamos/tests/Testes.java
@@ -7,7 +7,14 @@ public class Testes {
     @Test
     public void assertCreateClient() {
         Client client = new Client("Olavo", "22/05/2022", "Paraíba");
-
         assertEquals("Olavo", client.getNome());
+    }
+
+    @Test
+    public void assertCreateFatura() {
+        Client client = new Client("Olavo", "22/05/2022", "Paraíba");
+        Fatura fatura = new Fatura(150, "30/10/2022", client.getGuid());
+
+        assertEquals(client.getGuid(), fatura.getCliente());
     }
 }
