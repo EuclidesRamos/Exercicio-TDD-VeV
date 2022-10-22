e14337a4e450dcc4c324bf4957f1cd05838dc402
EuclidesRamos <euclides.filho@ccc.ufcg.edu.br>
2022-10-22 01:22:02 -0300

    criando caso de teste para testar o filtro de faturas para faturas com valor abaixo de 2000

--- FiltroFaturas-EuclidesRamos/tests/Testes.java
+++ FiltroFaturas-EuclidesRamos/tests/Testes.java
@@ -7,6 +7,7 @@ public class Testes {
     @Test
     public void assertCreateClient() {
         Client client = new Client("Olavo", "22/05/2022", "Paraíba");
+
         assertEquals("Olavo", client.getNome());
     }
 
@@ -16,5 +17,15 @@ public class Testes {
         Fatura fatura = new Fatura(150, "30/10/2022", client.getGuid());
 
         assertEquals(client.getGuid(), fatura.getCliente());
+    }
+
+    @Test
+    public void assertFiltraFaturasComValorAbaixoDeDoisMil() {
+        Fatura fatura = new Fatura(150, "30/10/2022", client.getGuid());
+        FiltroFaturas filtro = new FiltroFaturas();
+        Fatura[] faturas = new Fatura[]{fatura};
+        Fatura[] arrayVazio = new Fatura[];        
+
+        assertEquals(arrayVazio, filtro.filtrar(faturas));
     }
 }
