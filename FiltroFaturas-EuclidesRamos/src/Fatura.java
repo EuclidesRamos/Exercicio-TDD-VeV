a68ad42c558726f0a7f8675ebdd50d7c1b2124bc
EuclidesRamos <euclides.filho@ccc.ufcg.edu.br>
2022-10-22 01:14:33 -0300

    criando atributos para a classe Fatura

--- FiltroFaturas-EuclidesRamos/src/Fatura.java
+++ FiltroFaturas-EuclidesRamos/src/Fatura.java
@@ -1,4 +1,14 @@
 public class Fatura {
+
+    private int codigo;
+    private int valor;
+    private String data;
+    private String cliente;
+
     public Fatura(int valor, String data, String cliente) {
+        this.codigo = 123;
+        this.valor = valor;
+        this.data = data;
+        this.cliente = cliente;
     }
 }
