87c1b1f72f5965beb1938ba887795afe3c4a6f40
Gustavo Gambarra <gustavo.gambarra@ccc.ufcg.edu.br>
2022-10-24 21:41:15 -0300

    fix: tester role name

--- CalculadoraDeSalario/src/tests/CalculatorTests.java
+++ CalculadoraDeSalario/src/tests/CalculatorTests.java
@@ -37,8 +37,8 @@ public class CalculatorTests {
 
     @Test
     void CalculateSalaryTester() {
-        Employee e = new Employee("Bob Esponja", "bob.esponja@siricascudo.com", 2000.0, "Tester");
-        Employee e2 = new Employee("Lula Molusco", "lula.molusco@siricascudo.com", 1500.0, "Tester");
+        Employee e = new Employee("Bob Esponja", "bob.esponja@siricascudo.com", 2000.0, "Testador");
+        Employee e2 = new Employee("Lula Molusco", "lula.molusco@siricascudo.com", 1500.0, "Testador");
 
         assertEquals(e.getSalary() * 0.75, Calculator.calculateSalary(e));
         assertEquals(e2.getSalary() * 0.85, Calculator.calculateSalary(e2));
